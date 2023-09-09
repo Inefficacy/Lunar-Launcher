6b3838ae59a5b84242e29dfec507ce749dc393ab
Inefficacy <inefficacyfr@gmail.com>
2023-09-09 17:16:10 -0600

    - fix

--- launch.py
+++ launch.py
@@ -1,9 +1,9 @@
 import json
 import os
-import requests
 import platform
 import re
 import subprocess
+from lunarclient import API
 
 os_type = platform.system()
 
@@ -24,77 +24,33 @@ def replace_variable(argument):
 			argument = argument.replace(f'%{v}%', variables[v])
 	return argument
 
-###
-# Modified from lcapi.py
-# https://github.com/Inefficacy/Lunar-Archiver/blob/main/lcapi.py
-###
-
-class LcAPI:
-	def __init__(self, req):
-		self.req = req
-	def download(self):
-		try:
-			r = requests.post('https://api.lunarclientprod.com/launcher/launch', json=self.req)
-		except requests.exceptions.RequestException as e:
-			return False
-		if r.status_code != 200 or r.json() == None:
-			return False
-		self.rjson = r.json()
-		return r
-	def fromFile(self, f):
-		self.rjson = json.load(f)
-		return self.rjson
-	def downloadArtifact(self, name):
-		for artifact in self.rjson['launchTypeData']['artifacts']:
-			if artifact['name'] == name:
-				try:
-					for a in self.rjson['launchTypeData']['artifacts']:
-						if a['name'] == name:
-							r = requests.get(a['url'])
-				except requests.exceptions.RequestException as e:
-					return False
-				if r.content == None:
-					return False
-				return r.content
-
 if config['launch']['mode'] in ['download', 'file']:
 	import io
 	import zipfile
+	API.getUpdate()
 	if config['launch']['mode'] == 'download':
-		api = LcAPI({
-			'hwid': 'na',
+		launch_response = API.getLaunch({
 			'os': { 'Linux': 'linux', 'Windows': 'win32', 'Darwin': 'darwin' }.get(os_type),
-			'arch': 'x64' if platform.machine().endswith('64') else 'x32',
-			'version': config['version'],
-			'branch': 'master',
-			'luanch_type': 'OFFLINE',
-			'classifier': 'optifine'
+			'os_release': platform.version(),
+			'arch': platform.machine().lower() if platform.machine().lower() in ['arm', 'arm64', 'ia32', 'mips', 'mipsel', 'ppc', 'ppc64', 's390', 's390x', 'x64'] else 'x64',
+			'version': config['version']
 		})
-		launch_request = api.download()
-		if launch_request == False:
-			print('Error with sending a launch request.')
-			exit()
-		launch_json = launch_request.json()
 	else:
-		api = LcAPI(None)
-		launch_json = api.fromFile(open(config['launch']['file']))
+		launch_response = json.load(open(config['launch']['file'], 'r'))
 
-	artifacts = [a['name'] for a in launch_json['launchTypeData']['artifacts']]
+	artifacts = API.parseArtifacts(launch_response)
 
 	savedir = modify_path(config['launch']['directory'])
 
 	os.makedirs(savedir, exist_ok=True)
 
-	for a in artifacts:
-		d = api.downloadArtifact(a)
-		if d == False:
-			print('Error downloading artifact.')
-			exit()
-		if 'natives' in a and a.endswith('.zip'):
-			with zipfile.ZipFile(io.BytesIO(d)) as zf:
+	for artifact in artifacts:
+		if 'natives' in artifact.name and artifact.name.endswith('.zip'):
+			with zipfile.ZipFile(io.BytesIO(artifact.download())) as zf:
 				zf.extractall(os.path.join(savedir, 'natives'))
-		with open(os.path.join(savedir, a), 'wb') as f:
-			f.write(d)
+		else:
+			with open(os.path.join(savedir, artifact.name), 'wb') as f:
+				f.write(artifact.download())
 
 jar_files = [f for f in os.listdir(modify_path(config['launch']['directory'])) if f.endswith('.jar')]
 
